bb24d62d8c21faeeb5e1251c0a684d8c3e52b520
Mrunal Deshmukh <mrunaldeshmukh@Mrugens-MacBook-Pro-2.local>
2020-04-03 01:25:25 -0700

    Added linkedlist optimization for malloc to get block from LinkedList

--- hello_world.c
+++ hello_world.c
@@ -1,11 +1,18 @@
 #include<stdio.h>
 #include "malloc.c"
 
-
-
 int main(int argc, char **argv) {
     int *p = (int *)malloc(10 * sizeof(int));
     p[0] = 7729;
     printf("\n value of p[0] = %d\n", p[0]);
+    free(p);
+    printf("\n value of p[0] after free = %d\n", p[0]);
+
+    //request again for same size block, it will return by linkedList.
+    p = (int *)malloc(10 * sizeof(int));
+    p[0] = 7729;
+    printf("\n value of p[0] = %d\n", p[0]);
+    free(p);
+    printf("\n value of p[0] after free = %d\n", p[0]);
     return 0;
 }
